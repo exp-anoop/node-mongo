debb611155246ecfc8f53399d988f2f021ed907a
Anoop P R <anoop.pr@experionglobal.com>
2017-01-31 11:46:58 +0530

    Changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://anoop:heyitsme@ds117859.mlab.com:17859/seed-application');
+// mongoose.connect('mongodb://anoop:heyitsme@ds117859.mlab.com:17859/seed-application');
+mongoose.connect('mongodb://localhost:27017/seed-application');
 
 module.exports = {mongoose};
